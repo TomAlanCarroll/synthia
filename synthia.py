b05ea3508bc0c8cc4f9e2bd7344ebcf8be4740c6
Sarah Carroll <scarroll@wayfair.com>
2016-12-09 21:38:42 +0100

    Create a route for message

--- synthia.py
+++ synthia.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, world, v7.'
+
+
+@app.route('/get_morning_message', methods=['POST'])
+def get_morning_message():
+    message = 'Not post'
+
+    if request.method == 'POST':
+        message = 'Post'
+
+    return message
