c2c29b2724661b8b1f8c39028c10ce3690721139
gkhorn <gkhorn978@gmail.com>
2016-12-09 22:27:35 +0100

    added comment to play_message.py

--- play_message.py
+++ play_message.py
@@ -2,6 +2,7 @@ from gtts import gTTS
 from playsound import playsound
 
 
+# This function takes a message and language(accent) and plays sound
 def play_message(message, language):
     audio_file = "/tmp/audio.mp3"
     text_to_message = gTTS(message, language)
