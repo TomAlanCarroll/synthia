2aa413fa3ccddcb0b8b1a2970f2f7cdf18c20c28
Sarah Carroll <scarroll@wayfair.com>
2016-12-09 23:14:19 +0100

    Make a call to the weather API to get current weather

--- synthia.py
+++ synthia.py
@@ -1,17 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, weather, requests, json
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world, v7.'
+    return 'Hello, my name is Synthia, the Synthetic Intelligent Assistant for your home'
 
 
 @app.route('/get_morning_message', methods=['POST'])
 def get_morning_message():
-    message = 'Not post'
+    message = 'Good morning!'
 
-    if request.method == 'POST':
-        message = 'Post'
+    result = requests.get(weather.API_END_POINT);
+    content = result.content
+
+    data = json.loads(content)
+    weather_data = data.get('weather')
+    current_weather = weather_data[0].get('main')
+
+    if current_weather:
+        message += 'The current weather is ' + current_weather
+        current_weather_reminder_message = get_current_weather_reminder_message(current_weather)
+
+        if current_weather_reminder_message:
+            message += current_weather_reminder_message
+
+    message += ' Have a nice day!'
 
     return message
+
+def get_current_weather_reminder_message(current_weather):
+    return {
+        weather.RAINING: ' Blah.',
+        weather.DRIZZLE: ' Don\'t forget your umbrella.',
+        weather.SUNNY: ' You should wear sunglasses.',
+    }.get(current_weather, '')
+
+
+print get_morning_message();
