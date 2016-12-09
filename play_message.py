33b8bdfc5d548f2d8277d5b7ac165e4efff37f94
gkhorn <gkhorn978@gmail.com>
2016-12-09 22:37:35 +0100

    allow sysarg to be passed as message and default language to en-us

--- play_message.py
+++ play_message.py
@@ -1,9 +1,10 @@
+import sys
 from gtts import gTTS
 from playsound import playsound
 
 
 # This function takes a message and language(accent) and plays sound
-def play_message(message, language):
+def play_message(message=sys.argv[0], language="en-us"):
     audio_file = "/tmp/audio.mp3"
     text_to_message = gTTS(message, language)
     text_to_message.save(audio_file)
