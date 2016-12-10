3f41f1a9c60ec272899e15cc3157f9261a086c39
gkhorn <gkhorn978@gmail.com>
2016-12-10 18:41:15 +0100

    changed audio playback command to mpg321 which honors system default playback device

--- play_message.py
+++ play_message.py
@@ -4,15 +4,17 @@ from subprocess import call
 from playsound import playsound
 
 
-def play_message(message, language="en"):
+def play_message(message, language="en-us"):
     """
     Plays message as audio through default audio device
     """
-    audio_file = "/tmp/audio.mp3"
+    audio_mp3_file = "/tmp/audio.mp3"
     text_to_message = gTTS(message, language)
-    text_to_message.save(audio_file)
+    text_to_message.save(audio_mp3_file)
+    #
     if sys.platform == "linux2":
         # using shell call as quick a dirty way to play mp3 on a raspberry pi
-        call(["omxplayer",audio_file])
+        call(["mpg321", audio_mp3_file])
+
     else:
-        playsound(audio_file)
+        playsound(audio_mp3_file)
