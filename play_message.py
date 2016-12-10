e3abef01c87287757baa011d934a37248c04bf14
gkhorn <gkhorn978@gmail.com>
2016-12-10 11:56:05 +0100

    added conditions for testing on RPi vs macbook

--- play_message.py
+++ play_message.py
@@ -1,5 +1,7 @@
+import sys
 from gtts import gTTS
 from subprocess import call
+from playsound import playsound
 
 
 def play_message(message, language="en-us"):
@@ -9,7 +11,8 @@ def play_message(message, language="en-us"):
     audio_file = "/tmp/audio.mp3"
     text_to_message = gTTS(message, language)
     text_to_message.save(audio_file)
-
-    # using shell call as quick a dirty way to play mp3 on a raspberry pi
-    call(["omxplayer",audio_file])
-
+    if sys.platform == "linux2":
+        # using shell call as quick a dirty way to play mp3 on a raspberry pi
+        call(["omxplayer",audio_file])
+    else:
+        playsound(audio_file)
