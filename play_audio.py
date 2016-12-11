4fd16507e8d47077edbdc6a95a54bb88bfa588bc
Sarah Carroll <scarroll@wayfair.com>
2016-12-11 11:36:43 +0100

    fix merge conflictS

--- play_audio.py
+++ play_audio.py
@@ -5,11 +5,8 @@ from gtts import gTTS
 from subprocess import call
 from playsound import playsound
 
-
+# Plays audio file default audio device
 def play_audio_file(audio_file):
-    """
-    Plays audio file default audio device
-    """
     # linux2 matches RPi system
     if sys.platform == "linux2":
         # using shell call as quick a dirty way to play mp3 on a raspberry pi
@@ -20,11 +17,8 @@ def play_audio_file(audio_file):
         print "playing audio file: " + audio_file
         playsound(audio_file)
 
-
+# Plays message as audio through default audio device
 def play_message(message, language="en-us"):
-    """
-    Plays message as audio through default audio device
-    """
     audio_mp3_file = "/tmp/audio.mp3"
     text_to_message = gTTS(message, language)
     text_to_message.save(audio_mp3_file)
--- weather.py
+++ weather.py
@@ -1,10 +1,8 @@
-API_KEY = '712b45e138a845aec19cf970dd14ffc5'
-CITY = 'Berlin'
 API_END_POINT = 'http://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=712b45e138a845aec19cf970dd14ffc5';
 CLEAR = 'Clear'
 SUNNY = 'Sunny'
 CLOUDY = 'Cloudy'
-RAINING = 'Raining'
+RAIN = 'Rain'
 DRIZZLE = 'Drizzle'
 COLD_TEMPERATURE = 5
 WARM_TEMPERATURE = 25
