46935386da74c3c87089acd6c09e24d291b4ce1a
Tom Carroll <tomalancarroll@gmail.com>
2017-02-01 22:19:18 +0100

    Improved weather messages

--- synthia_controller.py
+++ synthia_controller.py
@@ -14,9 +14,9 @@ def play_morning_message():
 
 # Gets a custom morning message with helpful tips to start your day
 def get_morning_message():
-    message = 'Good morning! '
+    message = 'Morning! '
 
-    result = requests.get(config.get("weather_endpoint"));
+    result = requests.get(config.get('weather_endpoint'))
     content = result.content
     data = json.loads(content)
 
@@ -25,30 +25,30 @@ def get_morning_message():
     current_weather_desc = weather_data[0].get('description')
 
     if current_weather:
-        message += 'The current weather is ' + current_weather_desc + " in Berlin. "
-        current_weather_reminder_message = get_current_weather_reminder_message(current_weather)
+        message += get_current_weather_message(current_weather_desc)
 
-        if current_weather_reminder_message:
-            message += current_weather_reminder_message
-
-    message += 'Have a nice day and don\'t forget your keys!'
+    message += 'Have a nice day. Don\'t forget your keys!'
 
     return message
 
 
-# Returns a message based off the current weather
-def get_current_weather_reminder_message(current_weather):
-    if current_weather == config.get("drizzle") or current_weather == config.get("rain"):
-        return 'You should bring an umbrella. '
+# Returns the current weather as an object of the sentence
+def get_current_weather_message(current_weather):
+    if current_weather == config.get('drizzle'):
+        return 'Expect light rain. '
 
-    if current_weather == config.get("sunny"):
-        return 'You should wear sunglasses. '
+    if current_weather == config.get('rain'):
+        return 'Expect rain; You should bring an umbrella. '
 
-    return ''
+    if current_weather == config.get('sunny'):
+        return 'It\'s gorgeous weather for a run. '
+
+    return None
 
 
 # Play a welcome home message
 def play_evening_message():
+    # TODO: Determine name from facial recognition
     message = get_welcome_home_message()
     audio_controller.play_message(message)
     play_song()
@@ -66,6 +66,6 @@ def get_welcome_home_message(name = ''):
 
 # Play a mp3 music file
 def play_song():
-    audio_file = "songs/evening.mp3"
+    audio_file = 'songs/evening.mp3'
     audio_controller.play_audio_file(audio_file)
 
