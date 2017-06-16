1f66b4ddffcb113e4abdb1406d3bdcbb726cdb32
Tom Carroll <tomalancarroll@gmail.com>
2017-06-16 23:59:23 +0200

    Started rekognition for faces

--- face/recognition.py
+++ face/recognition.py
@@ -1,1 +1,10 @@
-# TODO: Add Facial Recognition
+import base64
+import pprint
+def recognize_face(image, user):
+    with open(image, 'rb') as source_image:
+		source_bytes = source_image.read()
+	response = client.search_faces_by_image(
+		CollectionId = user,
+		Image = {'Bytes' : source_bytes}
+	)
+    pprint.pprint(response)
